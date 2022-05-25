52713dfa1637b357473098797f88f4a4d169f947
SlavenDj <53652906+SlavenDj@users.noreply.github.com>
2022-05-25 14:46:25 +0200

    app.js minor changes

--- client/src/app.js
+++ client/src/app.js
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Login } from './pages/login';
 import { Register } from './pages/register';
 import { Page404 } from './pages/page404';
 import { Home } from './pages/home';
 import { ProtectedPage } from './pages/protectedPage';
+
 import ProtectedRoute from './components/ProtectedRoute';
 
 export const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home />} /> 
+        {/* do we need index parametar? */}
         <Route path='*' element={<Page404 />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
@@ -23,6 +26,6 @@ export const App = () => {
           }
         />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
