8f3928c3d8ba5febd3f1d61008c7e1285866189f
Savo Kos <savo.kos.sk@gmail.com>
2022-05-29 14:07:10 +0200

    Added better error handling

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,65 +1,21 @@
 const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
 
-const handleErrors = (err) => {
-  console.log(err.message);
-  let errors = { email: '', password: '' };
+module.exports.getUser = catchAsync(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  res.status(200).json({ status: 'success', data: user });
+});
 
-  // incorrect email
-  if (err.message === 'Incorrect email') {
-    errors.email = 'That email is not registered';
-  }
+module.exports.updateUser = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (Object.keys(req.body).length === 0) throw new Error('Enter user data!');
+  res.status(200).json({ status: 'success', data: user });
+});
 
-  // incorrect password
-  if (err.message === 'Incorrect password') {
-    errors.password = 'That password is incorrect';
-  }
-
-  // duplicate email error
-  if (err.code === 11000) {
-    errors.email = 'That email is already registered';
-    return errors;
-  }
-
-  // validation errors
-  if (err.message.includes('User validation failed')) {
-    Object.values(err.errors).forEach(({ properties }) => {
-      errors[properties.path] = properties.message;
-    });
-  }
-
-  return errors;
-};
-
-module.exports.getUser = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    res.status(200).json({ status: 'success', data: user });
-  } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
-};
-
-module.exports.updateUser = async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (Object.keys(req.body).length === 0) throw new Error('Enter user data!');
-    res.status(200).json({ status: 'success', data: user });
-  } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
-};
-
-module.exports.deleteUser = async (req, res) => {
-  try {
-    await Tour.findByIdAndDelete(req.params.id);
-    res.status(204).json({ status: 'success', data: null });
-  } catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
-};
+module.exports.deleteUser = catchAsync(async (req, res, next) => {
+  await Tour.findByIdAndDelete(req.params.id);
+  res.status(204).json({ status: 'success', data: null });
+});
--- server/index.js
+++ server/index.js
@@ -4,9 +4,17 @@ const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const AppError = require('./utils/appError');
+const errorController = require('./controllers/errorController');
 
 const app = express();
 const PORT = process.env.PORT;
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION. Shutting down...');
+  server.close(() => process.exit(1));
+});
 
 const db = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -24,13 +32,13 @@ app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/users', authRoutes);
+app.all('*', (req, res, next) => {
+  console.log('aaa');
+  next(new AppError(`Route ${req.originalUrl} is not defined!`, 404));
+});
+
+app.use(errorController);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
-});
-
-process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
-  console.log('UNHANDLED REJECTION. Shutting down...');
-  server.close(() => process.exit(1));
 });
