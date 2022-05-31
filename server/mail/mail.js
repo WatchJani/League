32e565176e225584bc453cd3f7bac21193a52dcc
WatchJani <jankokondic84@gmail.com>
2022-05-31 16:35:59 +0200

    + mail name

--- server/mail/mail.js
+++ server/mail/mail.js
@@ -11,12 +11,15 @@ const transport = nodemailer.createTransport({
 module.exports.sendConfirmationEmail = (email, confirmationCode) => {
     //https://betterprogramming.pub/how-to-create-a-signup-confirmation-email-with-node-js-c2fea602872a
     //trebaju jos neke izmjene u modelu kako bi ovo sve lijepo radilo
+
+    let name = email.substring(0, email.lastIndexOf("@"));
+
     transport.sendMail({
         from: process.env.MAIL_USER,
         to: email,
         subject: "Please confirm your account",
         html: `<h1>Email Confirmation</h1>
-          <h2>Hello new User</h2>
+          <h2>Hello ${name}</h2>
           <p>Please confirm your email by clicking on the following link</p>
           <a href=http://localhost:5000/register/${confirmationCode}> http://localhost:5000/register/${confirmationCode}</a>
           </div>`,
